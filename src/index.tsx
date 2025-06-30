9d7ce1e18c0609d304c26af9e0ad015129d5c136
Lucas <lucas.falkowsky@typedigital.de>
2025-06-30 14:14:37 +0200

    fix: export plugins

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { TelemetryDeckContext } from "./telemetrydeck-context";
 import { TelemetryDeckProvider } from "./telemetrydeck-provider";
 import { createTelemetryDeck } from "./create-telemetrydeck";
 import { useTelemetryDeck } from "./use-telemetrydeck";
+import { browserPlugin } from "./plugins";
 
 export {
   TelemetryDeckContext,
   TelemetryDeckProvider,
   createTelemetryDeck,
   useTelemetryDeck,
+  browserPlugin,
 };
