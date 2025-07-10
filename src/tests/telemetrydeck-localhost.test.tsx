f5c7ae3afe128901ffd097e0c1ac4911f5ef5888
Lucas <lucas.falkowsky@typedigital.de>
2025-07-10 13:53:51 +0200

    test: signals is test mode prop for localhost

--- src/tests/telemetrydeck-localhost.test.tsx
+++ src/tests/telemetrydeck-localhost.test.tsx
@@ -1,28 +1,114 @@
+/* eslint-disable import/no-unassigned-import */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable max-len */
+import { setupServer } from "msw/lib/node";
+import { renderHook } from "@testing-library/react";
+import React from "react";
+import { TelemetryDeckProvider } from "../telemetrydeck-provider";
+import { useTelemetryDeck } from "../use-telemetrydeck";
 import { createTelemetryDeck } from "../create-telemetrydeck";
+import { handlers } from "./test-utils/handlers";
 import { appID } from "./test-utils/variables";
+import "./__mocks__/mock-global";
+import "cross-fetch/polyfill";
 
-let windowSpy: any;
+describe("localhost Signal an die API", () => {
+  const server = setupServer(...handlers);
+  const originalLocation = window.location;
 
-beforeEach(() => {
-  windowSpy = jest.spyOn(window, "window", "get");
-});
-afterEach(() => {
-  windowSpy!.mockRestore();
-});
+  const mockLocation = (hostname: string) => {
+    Object.defineProperty(window, "location", {
+      writable: true,
+      value: {
+        ...originalLocation,
+        hostname,
+      },
+    });
+  };
 
-test("Given the telemetryDeck was initilized, when the environment is a localhost, then telemetryDeck should be in testMode", () => {
-  const td = createTelemetryDeck({ appID, clientUser: "anonymous" });
-  expect(td.testMode).toBeTruthy();
-});
-test("Given the telemetryDeck was initilized, when the environment is not a production, then telemetryDeck should not be in testMode", () => {
-  windowSpy.mockImplementation(() => ({
-    location: {
-      hostname: "https://example.com",
-    },
-  }));
-  const td = createTelemetryDeck({ appID, clientUser: "anonymous" });
-  expect(td.testMode).toBeFalsy();
+  beforeAll(() => server.listen());
+  beforeEach(() => {
+    delete (window as any).location;
+  });
+  afterEach(() => {
+    Object.defineProperty(window, "location", {
+      writable: true,
+      value: originalLocation,
+    });
+    server.resetHandlers();
+  });
+  afterAll(() => server.close());
+
+  test("Given the telemetryDeck was initilized, when the environment is not a production, then telemetryDeck should not be in testMode", () => {
+    mockLocation("example.host");
+    const td = createTelemetryDeck({ appID, clientUser: "anonymous" });
+    expect(td.testMode).toBeFalsy();
+  });
+  test("Given the telemetryDeck was initilized, when the environment is a localhost, then telemetryDeck should be in testMode", () => {
+    mockLocation("localhost");
+    const td = createTelemetryDeck({ appID, clientUser: "anonymous" });
+    expect(td.testMode).toBeTruthy();
+  });
+
+  it("Given the telemetryDeck was initialized on production, when sending a signal, then the signal request body should include isTestMode to be true", async () => {
+    mockLocation("example.host");
+    const td = createTelemetryDeck({
+      appID,
+      clientUser: "anonymous-tester",
+    });
+
+    let isTestmode = false;
+    server.events.on("request:start", (request) => {
+      const { body } = request;
+      if (Array.isArray(body) && body.length > 0) {
+        const [{ isTestMode }] = body;
+        isTestmode = isTestMode;
+      } else {
+        const { isTestMode } = body as { isTestMode: boolean };
+        isTestmode = isTestMode;
+      }
+    });
+
+    const Wrapper = ({ children }: { children: React.ReactNode }) => (
+      <TelemetryDeckProvider telemetryDeck={td}>
+        {children}
+      </TelemetryDeckProvider>
+    );
+
+    const { result: { current: { signal } } } = renderHook(() => useTelemetryDeck(), { wrapper: Wrapper });
+    await signal("signal button click");
+
+    expect(isTestmode).toBeFalsy();
+  });
+  it("Given the telemetryDeck was initialized on localhost, when sending a signal, then the signal request body should include isTestMode to be false", async () => {
+    mockLocation("localhost");
+    const td = createTelemetryDeck({
+      appID,
+      clientUser: "anonymous-tester",
+    });
+
+    let isTestmode = false;
+    server.events.on("request:start", (request) => {
+      const { body } = request;
+      if (Array.isArray(body) && body.length > 0) {
+        const [{ isTestMode }] = body;
+        isTestmode = isTestMode;
+      } else {
+        const { isTestMode } = body as { isTestMode: boolean };
+        isTestmode = isTestMode;
+      }
+    });
+
+    const Wrapper = ({ children }: { children: React.ReactNode }) => (
+      <TelemetryDeckProvider telemetryDeck={td}>
+        {children}
+      </TelemetryDeckProvider>
+    );
+
+    const { result: { current: { signal } } } = renderHook(() => useTelemetryDeck(), { wrapper: Wrapper });
+    await signal("signal button click");
+
+    expect(isTestmode).toBeTruthy();
+  });
 });
